213c930c0850438a098d519ef76ebf25f0f3aac7
lovesh <lovesh.bond@gmail.com>
2022-05-03 19:11:25 +0530

    Fix imports
    Signed-off-by: lovesh <lovesh.bond@gmail.com>

--- src/accumulator/index.ts
+++ src/accumulator/index.ts
@@ -4,6 +4,3 @@ export * from './accumulatorWitness';
 export * from './proof';
 export * from './IAccumulatorState';
 export * from './IInitialElementsStore';
-export { AccumulatorPublicKey } from './params-and-keys';
-export { AccumulatorSecretKey } from './params-and-keys';
-export { AccumulatorParams } from './params-and-keys';
--- src/composite-proof/index.ts
+++ src/composite-proof/index.ts
@@ -3,4 +3,3 @@ export * from './witness';
 export * from './proof';
 export * from './proof-spec';
 export * from './setup-param';
-export { ProofSpecG1 } from './proof-spec';
