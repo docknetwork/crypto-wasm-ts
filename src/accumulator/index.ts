529f31a9dff5710b6959126ed3c00831064aac5a
lovesh <lovesh.bond@gmail.com>
2024-03-26 16:09:46 +0530

    Upgrade deps
    Signed-off-by: lovesh <lovesh.bond@gmail.com>

--- src/accumulator/index.ts
+++ src/accumulator/index.ts
@@ -4,4 +4,6 @@ export * from './accumulatorWitness';
 export * from './proof';
 export * from './IAccumulatorState';
 export * from './IInitialElementsStore';
-export { VBWitnessUpdateInfo } from './witness-update-info';
+export * from './kb-universal-accumulator';
+export * from './kb-acccumulator-witness';
+export { VBWitnessUpdateInfo, KBUniversalMembershipWitnessUpdateInfo, KBUniversalNonMembershipWitnessUpdateInfo } from './witness-update-info';
