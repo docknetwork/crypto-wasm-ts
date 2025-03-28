823e55177828b5f58368f18ff4a6b41d2690e59a
Ahmed Elghareeb <ahmed.elghareeb@proton.me>
2025-03-28 17:24:40 +0200

    fix: adds missing export for InMemory accumulators

--- src/accumulator/index.ts
+++ src/accumulator/index.ts
@@ -6,6 +6,7 @@ export * from './IAccumulatorState';
 export * from './IInitialElementsStore';
 export * from './kb-universal-accumulator';
 export * from './kb-acccumulator-witness';
+export * from './in-memory-persistence';
 export {
   VBWitnessUpdateInfo,
   KBUniversalMembershipWitnessUpdateInfo,
